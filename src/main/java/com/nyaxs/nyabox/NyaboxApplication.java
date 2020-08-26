4fcfee36ab92ac89ab4f3c6752379c1589925a41
LiuYonghui <1607746993@qq.com>
2020-08-26 17:16:22 +0800

    first commit

---
+++ src/main/java/com/nyaxs/nyabox/NyaboxApplication.java
@@ -0,0 +1,13 @@
+package com.nyaxs.nyabox;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NyaboxApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NyaboxApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/nyaxs/nyabox/NyaboxApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nyaxs.nyabox;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NyaboxApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
